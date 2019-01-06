23e27e8c02266869431d4a1a4b69b5c551e55b3e
nikitagorsky <nicki070898@gmail.com>
2019-01-06 20:44:36 +0300

    Extended greeting.

--- vishlist.py
+++ vishlist.py
@@ -39,6 +39,8 @@ class Crud(QWidget):
 
 		self.table = QTableView(self)
 
+
+		# Данные для подключения к БД
 		self.db = QtSql.QSqlDatabase.addDatabase('QMYSQL')
 		self.db.setHostName('localhost')
 		self.db.setDatabaseName('DBvishlist')
